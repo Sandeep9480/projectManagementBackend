6be6fd161cd597058836c00c16b1a08cc34c06fb
Sandeep9480 <160817626+Sandeep9480@users.noreply.github.com>
2024-12-13 23:30:29 +0530

    Changed Port

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(projectRoutes);
 app.use(userRoutes);
 
-const Port = process.env.PORT || 400;
+const Port = process.env.PORT || 4000;
 
 main()
   .then(() => {
